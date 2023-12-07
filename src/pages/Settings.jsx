c0fc70d4366d2aceeb4a17dde10fd6c09b4fd983
1835917 <1835917@students.wits.ac.za>
2023-12-07 17:04:39 +0200

    capp frontend deployment

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setProfile } from "../Profileslice";
 import useAxiosPrivate from "../hooks/useAxiosPrivate.jsx";
 import { jwtDecode } from "jwt-decode";
